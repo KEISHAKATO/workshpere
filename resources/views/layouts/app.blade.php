9a2526fea9b5c2fb88eacc32106dfbb33ba80376
KEISHAKATO <keisha.kato@strathmore.edu>
2025-09-12 22:15:07 +0300

    feat(ui): add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,12 +8,12 @@
 
     <title>{{ $title ?? config('app.name', 'Worksphere') }}</title>
 
+    
     {{-- Fonts --}}
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@300..700" rel="stylesheet">
 
-    {{-- Expose Google Maps key for autocomplete --}}
     <script>window.WORKSPHERE_GOOGLE_KEY = @json(config('services.google.maps_key'));</script>
 
     {{-- Vite --}}
